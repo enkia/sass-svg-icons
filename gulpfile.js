c5c67e6a0b8ef519a902405b8a683d4d70e43941
enkia <enkia@darkfires.com>
2016-02-08 18:41:54 -0500

    -

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,20 @@ var gulp = require('gulp');
     imagemin = require('gulp-imagemin');
     watch = require('gulp-watch');
     open = require('gulp-open');
+    svgmin = require('gulp-svgmin');
+    urlencode = require('urlencode-stream');
 
 // path variables
 path = {
     assets: {
-        src: './source/assets/svg/*.svg',
-        dest: './source/assets/svg/processed'
+        src: './source/svg/*.svg',
+        dest: './source/processed'
     }
 };
 
 // default and build tasks
 gulp.task('default', ['watch']);
 gulp.task('svg', ['build-svg']);
-
-// watch for events
-/*gulp.task('watch', function () {
-    gulp.watch(path.assets.src, ['build-svg']);
-});*/
 
 gulp.task('watch', function () {
     watch(path.assets.src, batch(function (events, done) {
@@ -32,14 +29,16 @@ gulp.task('watch', function () {
 
 
 // optimize svg and open in editor
+var customURIGenerator = function(data){
+    return encodeURIComponent(data).replace(/%2F/g, "/");
+};
 gulp.task('build-svg', function() {
     return gulp.src(path.assets.src)
     .pipe(watch(path.assets.src))
     .pipe(cache('svg'))
-    .pipe(imagemin({
-        optimizationLevel: 5,
-        svgoPlugins: [{removeViewBox: false}]
+    .pipe(svgmin({
     }))
+    //.pipe(new urlencode())
     .pipe(gulp.dest(path.assets.dest))
     .pipe(open({app: 'Sublime Text'}));
 });
