c9622fc29881b125c175382e35e64abe899f13a5
enkia <enkia@darkfires.com>
2016-02-09 21:45:27 -0500

    -

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,74 @@
 // load gulp dependancies
 var gulp = require('gulp');
     gutil = require('gulp-util');
-    batch = require('gulp-batch');
+    browserSync = require('browser-sync').create();
     cache = require('gulp-cached');
     imagemin = require('gulp-imagemin');
     watch = require('gulp-watch');
     open = require('gulp-open');
-    svgmin = require('gulp-svgmin');
     sass = require('gulp-sass');
-    //urlencode = require('urlencode-stream');
+    rimraf = require('gulp-rimraf');
+    rename = require('gulp-rename');
 
-// path variables
+//  config variables
+editor = 'Sublime Text';
 path = {
     dist: './dist/**/*.scss',
     assets: {
-        src: './source/svg/*.svg',
-        dest: './source/processed'
+        src: './source/*.svg',
+        dest: './source/svg'
     },
     test: {
         sass: './test/scss/*.scss',
-        dest: './test'
+        dest: './test',
+        index: 'test.html'
     }
 };
 
-// default and build tasks
-gulp.task('default', ['watch']);
-gulp.task('svg', ['build-svg']);
+// default watch task
+gulp.task('default', ['watch', 'browser-sync']);
+
+// initialize browsersync
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: path.test.dest,
+        index: path.test.index,
+        browser: "safari",
+        notify: false
+    });
+});
 
 gulp.task('watch', function () {
-    watch(path.assets.src, batch(function (events, done) {
-        gulp.start('build-svg', done);
-    }));
+    watch(path.assets.src, function() {
+        gulp.start('build-svg');
+        gulp.start('open-file');
+    });
     gulp.watch(path.dist, ['build-css']);
     gulp.watch(path.test.sass, ['build-css']);
 });
 
-// optimize svg and open in editor
+
 var customURIGenerator = function(data){
     return encodeURIComponent(data).replace(/%2F/g, "/");
 };
+
+// optimize svg and open in editor
 gulp.task('build-svg', function() {
     return gulp.src(path.assets.src)
-    .pipe(watch(path.assets.src))
     .pipe(cache('svg'))
     .pipe(imagemin())
-    //.pipe(new urlencode())
-    .pipe(gulp.dest(path.assets.dest))
-    .pipe(open({app: 'Sublime Text'}));
+    .pipe(rename(function(path) {
+        path.basename = 'icon';
+    }))
+    .pipe(gulp.dest(path.assets.dest));
 });
 
+// open new svg file in editor
+gulp.task('open-file', ['build-svg'], function() {
+    gulp.src(path.assets.dest + '/icon.svg')
+    .pipe(open({app: editor}));
+    return
+});
 
 // build CSS Test file
 gulp.task('build-css', function() {
@@ -56,7 +76,12 @@ gulp.task('build-css', function() {
     .pipe(cache('scss'))
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(path.test.dest + '/css'))
+    .pipe(browserSync.stream());
 });
 
+gulp.task('clean', function() {
+    return gulp.src('./source/**/*.svg', { read: false }) // much faster
+    .pipe(rimraf());
+});
 
 
